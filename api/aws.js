1165825de875ea83f41ec5b62ebc494ee84f1206
mby444 <bimayudha161@gmail.com>
2024-02-10 17:21:59 +0700

    Migrate AWS SDK v2 to v3

--- api/aws.js
+++ api/aws.js
@@ -1,7 +1,7 @@
-import AWS from "aws-sdk";
+import { S3 } from "@aws-sdk/client-s3";
 import "../config/dotenv.js";
 
-const s3 = new AWS.S3();
+const s3 = new S3();
 const bucket = process.env.AWS_BUCKET;
 
 export const uploadFileToS3 = async (fileBuffer, fileFullPath) => {
@@ -11,7 +11,7 @@ export const uploadFileToS3 = async (fileBuffer, fileFullPath) => {
       Key: fileFullPath,
       Body: fileBuffer,
     };
-    const result = await s3.putObject(params).promise();
+    const result = await s3.putObject(params);
     console.log("uploadFileToS3", result);
   } catch (err) {
     console.log("uploadFileToS3", err);
